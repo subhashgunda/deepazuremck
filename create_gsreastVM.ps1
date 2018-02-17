e49314a3ca062c6843ed7a38efcafcad45fee16b
subhashgunda <subhash.gunda@mckesson.com>
2018-02-16 20:06:13 -0500

    first commit

---
+++ create_gsreastVM.ps1
@@ -0,0 +1,57 @@
+# Variables    
+## Global
+$ResourceGroupName = "gsrdeepazureeast"
+$Location = "eastus"
+
+## Storage
+$StorageName = "gsrstorageeast"
+$StorageType = "Standard_LRS"
+
+## Network
+$InterfaceName = "gsreastServerInterface"
+$Subnet1Name = "gsreastSubnet"
+$VNetName = "gsreastVNet"
+$VNetAddressPrefix = "10.0.0.0/16"
+$VNetSubnetAddressPrefix = "10.0.0.0/24"
+$NSGName = "gsreastnsg"
+
+## Compute
+$VMName = "gsreastVM"
+$ComputerName = "gsreastVM"
+$VMSize = "Standard_A2"
+$OSDiskName = $VMName + "OSDisk"
+
+# Login to azure portal
+Login-AzureRmAccount
+
+# Select the desired subscription
+Select-AzureRmSubscription -SubscriptionName "McKesson Deep Dive Training (4)"
+
+# Resource Group
+New-AzureRmResourceGroup -Name $ResourceGroupName -Location $Location
+
+# Storage
+$StorageAccount = New-AzureRmStorageAccount -ResourceGroupName $ResourceGroupName -Name $StorageName -Type $StorageType -Location $Location
+
+# Network
+$PIp = New-AzureRmPublicIpAddress -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -AllocationMethod Static
+$rule1 = New-AzureRmNetworkSecurityRuleConfig -Name rdp-rule -Description "Allow RDP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 100 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 3389
+$rule2 = New-AzureRmNetworkSecurityRuleConfig -Name web-rule -Description "Allow HTTP" -Access Allow -Protocol Tcp -Direction Inbound -Priority 101 -SourceAddressPrefix Internet -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 80
+$nsg = New-AzureRmNetworkSecurityGroup -ResourceGroupName $ResourceGroupName -Location $Location -Name $NSGName -SecurityRules $rule1,$rule2
+$SubnetConfig = New-AzureRmVirtualNetworkSubnetConfig -Name $Subnet1Name -AddressPrefix $VNetSubnetAddressPrefix -NetworkSecurityGroup $nsg
+$VNet = New-AzureRmVirtualNetwork -Name $VNetName -ResourceGroupName $ResourceGroupName -Location $Location -AddressPrefix $VNetAddressPrefix -Subnet $SubnetConfig
+$Interface = New-AzureRmNetworkInterface -Name $InterfaceName -ResourceGroupName $ResourceGroupName -Location $Location -SubnetId $VNet.Subnets[0].Id -PublicIpAddressId $PIp.Id
+
+# Compute
+
+## Setup local VM object
+$Credential = Get-Credential
+$VirtualMachine = New-AzureRmVMConfig -VMName $VMName -VMSize $VMSize
+$VirtualMachine = Set-AzureRmVMOperatingSystem -VM $VirtualMachine -Windows -ComputerName $ComputerName -Credential $Credential -ProvisionVMAgent -EnableAutoUpdate
+$VirtualMachine = Set-AzureRmVMSourceImage -VM $VirtualMachine -PublisherName MicrosoftWindowsServer -Offer WindowsServer -Skus 2012-R2-Datacenter -Version "latest"
+$VirtualMachine = Add-AzureRmVMNetworkInterface -VM $VirtualMachine -Id $Interface.Id
+$OSDiskUri = $StorageAccount.PrimaryEndpoints.Blob.ToString() + "vhds/" + $OSDiskName + ".vhd"
+$VirtualMachine = Set-AzureRmVMOSDisk -VM $VirtualMachine -Name $OSDiskName -VhdUri $OSDiskUri -CreateOption FromImage
+
+## Create the VM in Azure
+New-AzureRmVM -ResourceGroupName $ResourceGroupName -Location $Location -VM $VirtualMachine
